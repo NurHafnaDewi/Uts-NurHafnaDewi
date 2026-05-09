7257a4d77dadc7e7a4e964307fe712802aa23e3d
NurHafnaDewi <ahfananan@gmail.com>
2026-05-09 21:58:07 +0700

    Add files via upload

---
+++ loginsubmit.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+include 'koneksi.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = mysqli_query($conn,
+
+"SELECT * FROM user
+WHERE username='$username'
+AND password='$password'"
+
+);
+
+$cek = mysqli_num_rows($query);
+
+if($cek > 0){
+
+    $_SESSION['username'] = $username;
+
+    header("location:menu.php");
+
+}else{
+
+    echo "Login gagal";
+
+}
+
+?>
